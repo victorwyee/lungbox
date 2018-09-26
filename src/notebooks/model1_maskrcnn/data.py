1f902401d498db8afe24e0ecf9782defd9ae7d10
Victor Yee <w.victoryee@gmail.com>
2018-09-26 03:47:30 -0700

    Make training data retrieval functions private class functions.

--- src/notebooks/model1_maskrcnn/data.py
+++ src/notebooks/model1_maskrcnn/data.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 """Initial model with Mask R-CNN: Data preparation."""
 
-import os
-import sys
 import random
 import utils
 
@@ -16,20 +14,11 @@ class TrainingData:
     DICOM_WIDTH = 1024
     DICOM_HEIGHT = 1024
 
-    # Get training labels
-    train_box_df = ingest.read_s3_df(
-        bucket=GlobalConfig.get('S3_BUCKET_NAME'),
-        file_key=GlobalConfig.get('S3_TRAIN_BOX_KEY'))
-    annotation_dict = ingest.parse_training_labels(
-        train_box_df=train_box_df,
-        train_image_dirpath=GlobalConfig.get('S3_STAGE1_TRAIN_IMAGE_DIR'))
-
-    # Get list of images (SLOW before caching)
-    image_df = ingest.parse_dicom_image_list(
-        bucket=GlobalConfig.get('S3_BUCKET_NAME'))
-
     def __init__(self, subset_size, validation_split):
         random.seed(GlobalConfig.get('RANDOM_SEED'))
+        self.train_box_df = self._retrieve_training_box_labels()
+        self.annotation_dict = self._retrieve_annotation_dict(self.train_box_df)
+        self.image_df = self._retrieve_dicom_image_list()
 
         # Split train/test sets
         patient_id_subset = sorted(list(
@@ -39,6 +28,24 @@ class TrainingData:
             validation_split=validation_split)
         self.patient_id_train = id_split['train_ids']
         self.patient_id_valid = id_split['valid_ids']
+
+    def _retrieve_training_box_labels(self):
+        train_box_df = ingest.read_s3_df(
+            bucket=GlobalConfig.get('S3_BUCKET_NAME'),
+            file_key=GlobalConfig.get('S3_TRAIN_BOX_KEY'))
+        return train_box_df
+
+    def _retrieve_annotation_dict(self, train_box_df):
+        annotation_dict = ingest.parse_training_labels(
+            train_box_df=train_box_df,
+            train_image_dirpath=GlobalConfig.get('S3_STAGE1_TRAIN_IMAGE_DIR'))
+        return annotation_dict
+
+    def _retrieve_dicom_image_list(self):
+        # Get list of images (SLOW before caching)
+        image_df = ingest.parse_dicom_image_list(
+            bucket=GlobalConfig.get('S3_BUCKET_NAME'))
+        return image_df
 
     def get_train_ids(self):
         return self.patient_id_train
