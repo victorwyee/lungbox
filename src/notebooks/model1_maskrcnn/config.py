042eea1ba507b79fdcb26f8b01771e12c659b5e1
Victor Yee <w.victoryee@gmail.com>
2018-09-26 03:45:01 -0700

    Update mask R-CNN configs.

--- src/notebooks/model1_maskrcnn/config.py
+++ src/notebooks/model1_maskrcnn/config.py
@@ -3,6 +3,7 @@
 
 import os
 import sys
+import numpy as np
 script_path = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(script_path + "/Mask_RCNN")
 from mrcnn.config import Config
@@ -48,28 +49,125 @@ class DetectorConfig(Config):
     dataset. Overrides values in the base Config class.
     """
 
-    # Give the configuration a recognizable name
     NAME = 'pneumonia'
 
-    # Train on 1 GPU and 8 images per GPU. We can put multiple images on each
-    # GPU because the images are small. Batch size is 8 (GPUs * images/GPU).
+    # NUMBER OF GPUs to use. When using only a CPU, this needs to be set to 1.
     GPU_COUNT = 1
-    IMAGES_PER_GPU = 8
 
+    # Number of images to train with on each GPU.
+    # A 12GB GPU can typically handle 2 images of 1024x1024px.
+    # Adjust based on your GPU memory and image sizes.
+    # Use the highest number that your GPU can handle for best performance.
+    # p2.xlarge : GPU memory = 12GB
+    # p2.8xlarge: GPU memory = 96GB
+    # p3.2xlarge: GPU memory = 16GB
+    # p3.8xlarge: GPU memory = 64GB
+    # Since the DICOM images are small, we can put multiple images on each GPU.
+    # Batch size is 8 (GPUs * images/GPU).
+    IMAGES_PER_GPU = 8                 # default: 2
+
+    # Number of training steps per epoch
+    # This doesn't need to match the size of the training set. Tensorboard
+    # updates are saved at the end of each epoch, so setting this to a
+    # smaller number means getting more frequent TensorBoard updates.
+    # Validation stats are also calculated at each epoch end and they
+    # might take a while, so don't set this too small to avoid spending
+    # a lot of time on validation stats.
+    STEPS_PER_EPOCH = 1000             # default: 1000
+
+    # Number of validation steps to run at the end of every training epoch.
+    # A bigger number improves accuracy of validation stats, but slows
+    # down the training.
+    VALIDATION_STEPS = 50              # default: 50
+
+    # Backbone network architecture
+    # Supported values are: resnet50, resnet101.
+    # You can also provide a callable that should have the signature
+    # of model.resnet_graph. If you do so, you need to supply a callable
+    # to COMPUTE_BACKBONE_SHAPE as well
     BACKBONE = 'resnet101'
 
-    NUM_CLASSES = 2  # background + 1 pneumonia classes
+    # Number of classification classes (including background)
+    NUM_CLASSES = 2                    # background + 1 pneumonia classes
 
-    IMAGE_MIN_DIM = 256
-    IMAGE_MAX_DIM = 256
-    RPN_ANCHOR_SCALES = (32, 64, 128, 256)
-    TRAIN_ROIS_PER_IMAGE = 32
-    MAX_GT_INSTANCES = 3
-    DETECTION_MAX_INSTANCES = 3
-    DETECTION_MIN_CONFIDENCE = 0.9
-    DETECTION_NMS_THRESHOLD = 0.1
+    # Input image resizing
+    IMAGE_RESIZE_MODE = "square"
+    IMAGE_MIN_DIM = 960                # default: 800  -- smaller cropper recommended
+    IMAGE_MAX_DIM = 1024               # default: 1024 -- max size of DICOM images
 
-    STEPS_PER_EPOCH = 100
+    # Number of color channels per image. RGB = 3, grayscale = 1, RGB-D = 4
+    # Changing this requires other changes in the code. See the WIKI for more
+    # details: https://github.com/matterport/Mask_RCNN/wiki
+    IMAGE_CHANNEL_COUNT = 1            # grayscale
+
+    # Image mean
+    MEAN_PIXEL = np.array([127.5])     # NEED TO UPDATE
+
+    # Length of square anchor side in pixels
+    RPN_ANCHOR_SCALES = (32, 64, 128, 256, 512)  # default: (32, 64, 128, 256, 512)
+
+    # Number of ROIs per image to feed to classifier/mask heads
+    # The Mask RCNN paper uses 512 but often the RPN doesn't generate
+    # enough positive proposals to fill this and keep a positive:negative
+    # ratio of 1:3. You can increase the number of proposals by adjusting
+    # the RPN NMS threshold.
+    TRAIN_ROIS_PER_IMAGE = 300         # default: 200 -- 1024/3, rounded down
+
+    # Percent of positive ROIs used to train classifier/mask heads
+    ROI_POSITIVE_RATIO = 0.33          # default: 0.33
+
+    # Shape of output mask
+    # To change this you also need to change the neural network mask branch
+    MASK_SHAPE = [28, 28]              # default: [28, 28]
+
+    # Maximum number of ground truth instances to use in one image
+    MAX_GT_INSTANCES = 4               # default: 100 -- up to 4 true boxes per image
+
+    # Max number of final detections
+    DETECTION_MAX_INSTANCES = 4        # default: 100 -- up to 4 true boxes per image
+
+    # Minimum probability value to accept a detected instance
+    # ROIs below this threshold are skipped
+    DETECTION_MIN_CONFIDENCE = 0.67    # default: 0.7 -- lower to increase recall
+
+    # Non-maximum suppression threshold for detection
+    DETECTION_NMS_THRESHOLD = 0.3      # default: 0.3
+
+    # Learning rate and momentum
+    # The Mask RCNN paper uses lr=0.02, but on TensorFlow it causes
+    # weights to explode. Likely due to differences in optimizer
+    # implementation.
+    LEARNING_RATE = 0.001              # default: 0.001
+    LEARNING_MOMENTUM = 0.9            # default: 0.9
+
+    # Weight decay regularization
+    WEIGHT_DECAY = 0.0001              # default: 0.0001
+
+    # Loss weights for more precise optimization.
+    # Can be used for R-CNN training setup.
+    LOSS_WEIGHTS = {
+        "rpn_class_loss": 1.,          # default: 1.
+        "rpn_bbox_loss": 1.,           # default: 1.
+        "mrcnn_class_loss": 1.,        # default: 1.
+        "mrcnn_bbox_loss": 1.,         # default: 1.
+        "mrcnn_mask_loss": 1.          # default: 1.
+    }
+
+    # Use RPN ROIs or externally generated ROIs for training
+    # Keep this True for most situations. Set to False if you want to train
+    # the head branches on ROI generated by code rather than the ROIs from
+    # the RPN. For example, to debug the classifier head without having to
+    # train the RPN.
+    USE_RPN_ROIS = True                # default: True
+
+    # Train or freeze batch normalization layers
+    #     None: Train BN layers. This is the normal mode
+    #     False: Freeze BN layers. Good when using a small batch size
+    #     True: (don't use). Set layer in training mode even when predicting
+    TRAIN_BN = False                   # default: False, since batch size is often small
+
+    # Gradient norm clipping
+    GRADIENT_CLIP_NORM = 5.0           # default: 5.0
 
     def get_attrs(self):
         """Return all configs in one string."""
