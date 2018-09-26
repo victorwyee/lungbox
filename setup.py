f3f437600f3dbe7b403bc853fa8fac957d61335a
Victor Yee <w.victoryee@gmail.com>
2018-09-26 03:44:25 -0700

    Add working setup.py and requirements for model1.

--- setup.py
+++ setup.py
@@ -1,35 +1,60 @@
-import setuptools
-from setuptools import setup
-from os import path
-import platform
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# File: setup.py
+"""
+To build:
+>> pip install -r requirements.txt
+>> python setup.py install
+"""
 
-version = int(setuptools.__version__.split('.')[0])
-assert version > 30, "Installation requires setuptools > 30"
+from os import path
+import pip
+import logging
+import pkg_resources
+import setuptools
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setuptools_version = int(setuptools.__version__.split('.')[0])
+assert setuptools_version > 30, "Installation requires setuptools > 30"
 
 this_dirpath = path.abspath(path.dirname(__file__))
 
 with open(path.join(this_dirpath, 'README.md'), 'rb') as f:
     long_description = f.read().decode('utf-8')
 
+
+def _parse_requirements(file_path):
+    pip_ver = pkg_resources.get_distribution('pip').version
+    pip_version = list(map(int, pip_ver.split('.')[:2]))
+    if pip_version >= [6, 0]:
+        raw = pip.req.parse_requirements(file_path,
+                                         session=pip.download.PipSession())
+    else:
+        raw = pip.req.parse_requirements(file_path)
+    return [str(i.req) for i in raw]
+
+
+# parse_requirements() returns generator of pip.req.InstallRequirement objects
+try:
+    install_reqs = _parse_requirements("requirements.txt")
+except Exception:
+    logging.warning('Fail load requirements file, so using default ones.')
+    install_reqs = []
+
 setup(
     name='lungbox',
-    version=__version__,   # noqa
+    version='0.0.1',
+    url='https://github.com/victorwyee/lungbox',
+    author='Victor W. Yee',
+    author_email='w.victoryee@gmail.com',
+    license='MIT',
     description='A bounding box predictor for pneumonia from chest x-rays',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    install_requires=[
-        "atlas==0.27.0",
-        "boto3==1.7.82",
-        "botocore==1.10.84",
-        "configs==3.0.3",
-        "imgaug==0.2.6",
-        "matplotlib==3.0.0",
-        "numpy==1.14.5",
-        "pandas==0.23.4",
-        "pydicom==1.1.0",
-        "scikit-image==0.13",  # older version to avoid massive antialiasing warnings
-        "streamlit==0.15.5",
-        "tornado==4.5.3"       # allows async within async
-    ],
+    python_requires='>=3.6',
+    install_requires=install_reqs,
     tests_require=['flake8', 'scikit-image'],
 )
